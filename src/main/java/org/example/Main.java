5ab91ee0c7d0f626ea41bd34b9f9a2b1badc4711
ecekarabalik <ece.karabalik@hotmail.com>
2025-09-07 20:16:55 +0300

    palindrom

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,81 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Testler
+        System.out.println(isPalindrome(-1221));
+        System.out.println(isPalindrome(707));
+        System.out.println(isPalindrome(11212));
+
+        System.out.println(isPerfectNumber(6));
+        System.out.println(isPerfectNumber(28));
+        System.out.println(isPerfectNumber(5));
+        System.out.println(isPerfectNumber(-1));
+
+        System.out.println(numberToWords(123));
+        System.out.println(numberToWords(1010));
+        System.out.println(numberToWords(-12));
+    }
+
+    public static boolean isPalindrome(int number) {
+        int original = Math.abs(number);
+        int reversed = 0;
+        int temp = original;
+
+        while (temp > 0) {
+            int lastDigit = temp % 10;
+            reversed = reversed * 10 + lastDigit;
+            temp /= 10;
+        }
+
+        return original == reversed;
+    }
+
+    public static boolean isPerfectNumber(int number) {
+        if (number < 1) {
+            return false;
+        }
+
+        int sum = 0;
+        for (int i = 1; i <= number / 2; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+
+        return sum == number;
+    }
+
+    public static String numberToWords(int number) {
+        if (number < 0) {
+            return "Invalid Value";
+        }
+
+        if (number == 0) {
+            return "Zero";
+        }
+
+        StringBuilder result = new StringBuilder();
+        String numStr = String.valueOf(number);
+
+        for (int i = 0; i < numStr.length(); i++) {
+            char digit = numStr.charAt(i);
+            switch (digit) {
+                case '0': result.append("Zero"); break;
+                case '1': result.append("One"); break;
+                case '2': result.append("Two"); break;
+                case '3': result.append("Three"); break;
+                case '4': result.append("Four"); break;
+                case '5': result.append("Five"); break;
+                case '6': result.append("Six"); break;
+                case '7': result.append("Seven"); break;
+                case '8': result.append("Eight"); break;
+                case '9': result.append("Nine"); break;
+            }
+            if (i < numStr.length() - 1) {
+                result.append(" ");
+            }
+        }
+
+        return result.toString();
     }
 }
--- src/test/java/ResultAnalyzer.java
+++ src/test/java/ResultAnalyzer.java
@@ -81,4 +81,3 @@ public class ResultAnalyzer implements TestWatcher, AfterAllCallback {
 
 
 }
-// 
